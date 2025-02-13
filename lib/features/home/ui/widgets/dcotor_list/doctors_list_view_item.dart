4e4a97c919e64ea32fae1edb7e8a4a692b472a9f
khaledtarek12 <tareqkhalid092@gmail.com>
2025-02-13 17:46:24 +0200

    added shamer loading

--- lib/features/home/ui/widgets/dcotor_list/doctors_list_view_item.dart
+++ lib/features/home/ui/widgets/dcotor_list/doctors_list_view_item.dart
@@ -1,8 +1,11 @@
 import 'package:bookin_appointment/core/helpers/spacing.dart';
+import 'package:bookin_appointment/core/themes/colors.dart';
 import 'package:bookin_appointment/core/themes/styles.dart';
 import 'package:bookin_appointment/features/home/data/models/specialization_response_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class DoctorsListViewItem extends StatelessWidget {
   const DoctorsListViewItem({super.key, this.doctorsModel});
@@ -15,31 +18,62 @@ class DoctorsListViewItem extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12.r),
-            child: Image.network(
-              "https://static.wikia.nocookie.net/five-world-war/images/6/64/Hisoka.jpg/revision/latest?cb=20190313114050",
+          CachedNetworkImage(
+            imageUrl:
+                "https://static.wikia.nocookie.net/five-world-war/images/6/64/Hisoka.jpg/revision/latest?cb=20190313114050",
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Shimmer.fromColors(
+                baseColor: ColorsManager.lightGrey,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            imageBuilder: (context, imageProvider) => Container(
               width: 110.w,
               height: 120.h,
-              fit: BoxFit.cover,
-              // loadingBuilder: (context, url, downloadProgress) {
-              //   return Shimmer.fromColors(
-              //     baseColor: ColorsManager.lightGrey,
-              //     highlightColor: Colors.white,
-              //     child: Container(
-              //       width: 110.w,
-              //       height: 120.h,
-              //       decoration: BoxDecoration(
-              //         shape: BoxShape.rectangle,
-              //         borderRadius: BorderRadius.circular(12.0),
-              //         color: Colors.white,
-              //       ),
-              //     ),
-              //   );
-              // },
-              errorBuilder: (context, url, error) => const Icon(Icons.error),
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
+          // ClipRRect(
+          //   borderRadius: BorderRadius.circular(12.r),
+          //   child: Image.network(
+          //     "https://static.wikia.nocookie.net/five-world-war/images/6/64/Hisoka.jpg/revision/latest?cb=20190313114050",
+          //     width: 110.w,
+          //     height: 120.h,
+          //     fit: BoxFit.cover,
+          //     // loadingBuilder: (context, url, downloadProgress) {
+          //     //   return Shimmer.fromColors(
+          //     //     baseColor: ColorsManager.lightGrey,
+          //     //     highlightColor: Colors.white,
+          //     //     child: Container(
+          //     //       width: 110.w,
+          //     //       height: 120.h,
+          //     //       decoration: BoxDecoration(
+          //     //         shape: BoxShape.rectangle,
+          //     //         borderRadius: BorderRadius.circular(12.0),
+          //     //         color: Colors.white,
+          //     //       ),
+          //     //     ),
+          //     //   );
+          //     // },
+          //     errorBuilder: (context, url, error) => const Icon(Icons.error),
+          //   ),
+          // ),
           horizontalSpace(16),
           Expanded(
             child: Column(
